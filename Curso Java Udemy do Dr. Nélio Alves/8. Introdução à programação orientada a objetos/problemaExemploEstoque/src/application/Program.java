2f341ba9a6eb1d4e062af8a62c8297d3eed7141d
lucasrsanches <lucasrodriguessanches@hotmail.com>
2023-01-27 21:06:21 -0300

    .

--- Curso Java Udemy do Dr. Nélio Alves/8. Introdução à programação orientada a objetos/problemaExemploEstoque/src/application/Program.java
+++ Curso Java Udemy do Dr. Nélio Alves/8. Introdução à programação orientada a objetos/problemaExemploEstoque/src/application/Program.java
@@ -19,7 +19,7 @@ public class Program {
 		System.out.println("Quantity in Stock: ");
 		product.quantity = sc.nextInt();
 		
-		System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+		System.out.println(product);
 		
 		sc.close();
 	}
