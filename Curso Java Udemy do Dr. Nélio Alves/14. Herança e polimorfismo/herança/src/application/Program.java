75934d2a1b813593c86114fd8c060d9dd29e32a9
Lucas Rodrigues <lucasrodriguessanches@hotmail.com>
2023-03-03 00:35:41 -0300

    polimorfismo

--- Curso Java Udemy do Dr. Nélio Alves/14. Herança e polimorfismo/herança/src/application/Program.java
+++ Curso Java Udemy do Dr. Nélio Alves/14. Herança e polimorfismo/herança/src/application/Program.java
@@ -44,5 +44,12 @@ public class Program {
 			System.out.println("Uptade");
 		}
 		
+		Account x = new Account(1020, "Alex", 1000.0);
+		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+		x.withdraw(50.0);
+		y.withdraw(50.0);
+		System.out.println(x.getBalance());
+		System.out.println(y.getBalance());
+		
 	}
 }
