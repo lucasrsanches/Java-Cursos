6def61b5b67e52afdedecc8c87e83c83e181f89f
Lucas Rodrigues <lucasrodriguessanches@hotmail.com>
2023-04-17 08:08:38 -0300

    criação de novas pastas, e revisão de Date

---
+++ Curso Java para Iniciantes do Guanabara/course/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.util.Date;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Date date = new Date();
+		System.out.println(date.toString());
+
+	}
+
+}
