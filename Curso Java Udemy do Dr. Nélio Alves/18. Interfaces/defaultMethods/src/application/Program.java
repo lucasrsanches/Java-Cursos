eb39eb6b9e10ff00a553453cd7ce7438fd906c22
Lucas Rodrigues <lucasrodriguessanches@hotmail.com>
2023-05-02 22:03:20 -0300

    generics

--- Curso Java Udemy do Dr. Nélio Alves/18. Interfaces/defaultMethods/src/application/Program.java
+++ Curso Java Udemy do Dr. Nélio Alves/18. Interfaces/defaultMethods/src/application/Program.java
@@ -12,18 +12,17 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("Amount: ");
 		double amount = sc.nextDouble();
 		System.out.print("Months: ");
 		int months = sc.nextInt();
-		
+
 		InterestService is = new BrazilInterestService(2.0);
 		double payment = is.payment(amount, months);
-		
+
 		System.out.println("Payment after " + months + " months:");
 		System.out.println(String.format("%.2f", payment));
-		
 		sc.close();
 	}
 }
